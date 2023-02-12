bc2fcac30b5c60c0fb372e138af7f92dbbd005bc
1iceberg1 <aleksander.iceberg1202@gmail.com>
2023-02-12 22:36:05 +0630

    fix eslint errors

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,6 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          {' '}
           Learn React
         </a>
       </header>
