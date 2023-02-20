f4dba94026b479754e663a8c61762546b7164dba
1iceberg1 <aleksander.iceberg1202@gmail.com>
2023-02-20 18:49:55 +0630

    add router and link in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import {
+  BrowserRouter, Routes, Route, Outlet, Link,
+} from 'react-router-dom';
 import Calculator from './components/calculator';
 import './App.css';
 
+function Navbar() {
+  return (
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/calculator">Calculator</Link></li>
+      <li><Link to="/quote">Quote</Link></li>
+      <Outlet />
+    </ul>
+  );
+}
+
 function App() {
   return (
-    <Calculator />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<div>Home</div>} />
+          <Route path="calculator" element={<Calculator />} />
+          <Route path="quote" element={<div>Quote</div>} />
+          <Route path="*" element={<div>Not found</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
