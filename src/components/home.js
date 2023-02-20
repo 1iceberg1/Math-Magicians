af293fcf7f0c24cac909c904d5541f0eb8fcc3e9
1iceberg1 <aleksander.iceberg1202@gmail.com>
2023-02-20 19:05:02 +0630

    fix eslint errors

--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,14 @@
-const Home = () => {
-    return (
-        <div>
-            <h1>Welcome to our page!</h1>
-            <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat id blanditiis recusandae harum ratione nam est repellat porro, voluptas iusto quod laborum at qui minus. Nobis alias cumque vero, aperiam repellendus veritatis dicta id odio non aspernatur eveniet rerum optio culpa distinctio nisi iusto tempora ea iste animi veniam quam?
-            </p>
-            <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates modi ut quasi fugiat doloribus aspernatur voluptatem quos sapiente optio, laborum, deserunt voluptatibus provident eum ea sint ullam qui voluptate veniam! Aspernatur nesciunt rerum distinctio officiis eius incidunt esse repudiandae molestiae!
-            </p>
-        </div>
-    )
-}
+const Home = () => (
+  <div>
+    <h1>Welcome to our page!</h1>
+    <p>
+      {/* eslint-disable max-len */}
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat id blanditiis recusandae harum ratione nam est repellat porro, voluptas iusto quod laborum at qui minus. Nobis alias cumque vero, aperiam repellendus veritatis dicta id odio non aspernatur eveniet rerum optio culpa distinctio nisi iusto tempora ea iste animi veniam quam?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates modi ut quasi fugiat doloribus aspernatur voluptatem quos sapiente optio, laborum, deserunt voluptatibus provident eum ea sint ullam qui voluptate veniam! Aspernatur nesciunt rerum distinctio officiis eius incidunt esse repudiandae molestiae!
+    </p>
+  </div>
+);
 
 export default Home;
