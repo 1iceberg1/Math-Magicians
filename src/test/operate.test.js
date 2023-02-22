a303b7806c7d92b4b6d48375083b316d07029ec1
1iceberg1 <aleksander.iceberg1202@gmail.com>
2023-02-22 20:24:48 +0630

    change the relevant test name

--- src/test/operate.test.js
+++ src/test/operate.test.js
@@ -4,13 +4,13 @@ describe('make operations', () => {
   test('adding 2 to 2', () => {
     expect(operate(2, 2, '+')).toBe('4');
   });
-  test('adding 2 to 2', () => {
+  test('multiplying 2 with 2', () => {
     expect(operate(2, 2, 'x')).toBe('4');
   });
-  test('adding 2 to 2', () => {
-    expect(operate(3, 2, '%')).toEqual('1');
+  test('dividing 6 by 2', () => {
+    expect(operate(6, 2, '÷')).toEqual('3');
   });
-  test('adding 2 to 2', () => {
+  test('substracting 10 to 2', () => {
     expect(operate(10, 2, '-')).toEqual('8');
   });
 });
