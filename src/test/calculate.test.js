aba9987b9832fac631a0f6afa8dc709455010f83
1iceberg1 <aleksander.iceberg1202@gmail.com>
2023-02-22 16:31:30 +0630

    fix eslint errors

--- src/test/calculate.test.js
+++ src/test/calculate.test.js
@@ -1,31 +1,31 @@
-import calculate from "../logic/calculate";
+import calculate from '../logic/calculate';
 
 describe('Testing calculate.js', () => {
-    test('should return null', () => {
-        const obj = {
-            total: 3,
-            next: 6,
-            operation: '+',
-        };
-        const buttonName = 'AC';
-        expect(calculate(obj, buttonName)).toEqual({
-            total: null,
-            next: null,
-            operation: null,
-        })
-    })
+  test('should return null', () => {
+    const obj = {
+      total: 3,
+      next: 6,
+      operation: '+',
+    };
+    const buttonName = 'AC';
+    expect(calculate(obj, buttonName)).toEqual({
+      total: null,
+      next: null,
+      operation: null,
+    });
+  });
 
-    test('should return the multiplication of numbers', () => {
-        const obj = {
-            total: 3,
-            next: 6,
-            operation: 'x',
-        };
-        const buttonName = '=';
-        expect(calculate(obj, buttonName)).toEqual({
-            total: '18',
-            next: null,
-            operation: null,
-        })
-    })
-})
+  test('should return the multiplication of numbers', () => {
+    const obj = {
+      total: 3,
+      next: 6,
+      operation: 'x',
+    };
+    const buttonName = '=';
+    expect(calculate(obj, buttonName)).toEqual({
+      total: '18',
+      next: null,
+      operation: null,
+    });
+  });
+});
