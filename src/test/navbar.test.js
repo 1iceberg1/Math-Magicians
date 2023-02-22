ea12bb7a0e96391a17b6ffc0111c152c06229fa0
1iceberg1 <aleksander.iceberg1202@gmail.com>
2023-02-22 16:44:46 +0630

    add navbar.test.js

---
+++ src/test/navbar.test.js
@@ -0,0 +1,14 @@
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import Navbar from '../components/navbar';
+
+it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+            <Navbar />
+        </BrowserRouter>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
