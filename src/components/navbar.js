c800bdd7ba9d718e972a03845e38e633abf03ef2
1iceberg1 <aleksander.iceberg1202@gmail.com>
2023-03-22 18:42:49 +0630

    add humburger menu

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Navbar = () => (
   <nav className="navbar" data-testid="navbar">
     <h1 className="navTitle">Math Magicians</h1>
     <ul className="navLink">
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/calculator">Calculator</Link></li>
-      <li><Link to="/quote">Quote</Link></li>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/calculator">Calculator</Link>
+      </li>
+      <li>
+        <Link to="/quote">Quote</Link>
+      </li>
     </ul>
+    <nav class="hamburger">
+      <span class="line"></span>
+      <span class="line"></span>
+      <span class="line"></span>
+    </nav>
   </nav>
 );
 
